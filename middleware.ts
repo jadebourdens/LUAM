64106286a72d123723e17db328f40aba54ceb8df
Your Name <you@example.com>
2026-06-09 20:51:10 +0200

    fix: update middleware matcher

--- middleware.ts
+++ middleware.ts
@@ -4,5 +4,5 @@ import { routing } from './i18n/routing'
 export default createMiddleware(routing)
 
 export const config = {
-  matcher: ['/', '/(en|vi)/:path*']
+  matcher: ['/((?!_next/static|_next/image|favicon.ico|api).*)']
 }
