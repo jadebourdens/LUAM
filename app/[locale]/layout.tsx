3ea54fbd167b90c2f142535750274432157db8fe
Your Name <you@example.com>
2026-06-23 19:22:40 +0200

    feat: add GTM and Google Analytics

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -5,6 +5,8 @@ import { notFound } from 'next/navigation';
 import { routing } from '@/i18n/routing';
 import SiteHeader from '@/components/site-header';
 import SiteFooter from '@/components/site-footer';
+// Ignore missing type declarations for global CSS import in this TSX file
+// @ts-ignore
 import '../globals.css';
 
 export async function generateMetadata({ params }: { params: Promise<{ locale: string }> }) {
@@ -28,10 +30,45 @@ export default async function LocaleLayout({
   const messages = await getMessages();
 
   return (
-    <NextIntlClientProvider messages={messages}>
-      <SiteHeader />
-      {children}
-      <SiteFooter />
-    </NextIntlClientProvider>
+    <html lang={locale}>
+      <head>
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-54XFQDFH');`
+          }}
+        />
+        {/* Google Analytics */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-38Q1EG55BD" />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-38Q1EG55BD');
+            `
+          }}
+        />
+      </head>
+      <body>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-54XFQDFH"
+            height="0" width="0" style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
+        <NextIntlClientProvider messages={messages}>
+          <SiteHeader />
+          {children}
+          <SiteFooter />
+        </NextIntlClientProvider>
+      </body>
+    </html>
   );
 }
